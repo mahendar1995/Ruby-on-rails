651701c0ee4861540ad47dc4bcca5594c4630cab
JD4912 <58825633+JD4912@users.noreply.github.com>
2022-04-27 16:35:57 +0530

    Update beforeInstall.sh

--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -1,7 +1,4 @@
-# REPO="https://github.com/JD4912/ruby_test.git"
-cd /var/www/
-git clone https://github.com/JD4912/ruby_test.git
-# git clone $REPO ~/$DIR
-# cd $DIR
-rm -rf Gemfile.lock
+git clone https://github.com/JD4912/ruby_test.git /var/www/ruby
+cd /var/www/ruby
+rm Gemfile.lock
 bundle install
