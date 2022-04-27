398f48cbf9b30e6b632837ad87eff586ba710e7a
JD4912 <58825633+JD4912@users.noreply.github.com>
2022-04-27 10:48:05 +0530

    Update beforeInstall.sh

--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -1,5 +1,5 @@
-REPO="https://github.com/NihalSatbhai/Demo-Rails.git"
-DIR="sample-rails"
+REPO="https://github.com/JD4912/ruby_test.git"
+DIR="/var/www/ruby"
 git clone $REPO ~/$DIR
 cd $DIR
 rm Gemfile.lock
