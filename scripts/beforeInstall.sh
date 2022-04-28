65d78bcf0bcf7bc03b415efee65eae62e867f454
Vaibhavi Yadav <84709348+Vaibhavi-07@users.noreply.github.com>
2022-04-28 12:11:52 +0530

    Update beforeInstall.sh

--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -2,4 +2,4 @@ rm -rf /home/ubuntu/ruby
 git clone https://github.com/Vaibhavi-07/ruby_test.git /home/ubuntu/ruby
 cd /home/ubuntu/ruby
 rm Gemfile.lock
-bash -lc "gem install bundler"
+bundle install
